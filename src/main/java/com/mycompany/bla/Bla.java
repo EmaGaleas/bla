8db7664dde2aaf652dfe35b9a0eea85b4992dfda
pcast <pcast@DESK-E>
2023-08-04 13:02:09 -0600

    bla

---
+++ src/main/java/com/mycompany/bla/Bla.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bla;
+
+/**
+ *
+ * @author pcast
+ */
+public class Bla {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
